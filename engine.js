ffe4116c28905573825db703d14a3149234e7c78
Odepax <odepaxforuma@gmail.com>
2019-02-24 13:04:17 +0100

    Reorganized project files

--- engine.js
+++ engine.js
@@ -1,98 +1,3 @@
-let _logCount = 500
-function log() {
-	if (--_logCount > 0) {
-		console.log(...arguments)
-	}
-}
-
-// -----------------------------------------------------------------
-
-const RED = "crimson"
-const ORANGE = "darkorange"
-const YELLOW = "gold"
-const GREEN = "greenyellow"
-const TEAL = "mediumseagreen"
-const BLUE = "deepskyblue"
-const PURPLE = "mediumpurple"
-const PINK = "hotpink"
-const WHITE = "ghostwhite"
-const LIGHT = "silver"
-const GREY = "gray"
-const DARK = "darkslategray"
-const BLACK = "black"
-
-const MILLI_SECOND = 0.001
-const MILLI_SECONDS = 0.001
-const SECOND = 1
-const SECONDS = 1
-const MINUTE = 60
-const MINUTES = 60
-
-const PI = Math.PI
-const abs = Math.abs
-const sign = Math.sign
-const sin = Math.sin
-const cos = Math.cos
-const atan = Math.atan
-const atan2 = Math.atan2
-const ceil = Math.ceil
-const round = Math.round
-const sqrt = Math.sqrt
-const square = x => x * x
-const min = Math.min
-const max = Math.max
-const absMin = (a, b) => abs(a) < abs(b) ? a : b
-const absMax = (a, b) => abs(a) > abs(b) ? a : b
-const rand = (min, max) => Math.random() * (max - min) + min
-const randBetween = (...values) => values[parseInt(Math.random() * values.length)]
-
-// -----------------------------------------------------------------
-
-Object.prototype.apply = function apply(init) {
-	if (typeof init == "function") {
-		init(this)
-	} else {
-		Object.assign(this, init)
-	}
-
-	return this
-}
-
-Document.prototype.parseElements = function parseElements(htmlString) {
-	const div = this.createElement("div")
-
-	div.innerHTML = htmlString
-
-	const identifiedElements = {}
-
-	for (const element of div.querySelectorAll("[id]")) {
-		identifiedElements[element.getAttribute("id")] = element
-		element.removeAttribute("id")
-	}
-
-	return [ div.children, identifiedElements ]
-}
-
-Node.prototype.appendChildren = function appendChildren(htmlCollection) {
-	for (const element of htmlCollection) {
-		this.appendChild(element)
-	}
-
-	return htmlCollection
-}
-
-CanvasRenderingContext2D.prototype.applyTransform = function applyTransform({ x, y, a = 0 }, rotate = true) {
-	this.translate(x, y)
-
-	rotate && this.rotate(a)
-}
-
-CanvasRenderingContext2D.prototype.resetTransform = function resetTransform() {
-	this.setTransform(1, 0, 0, 1, 0, 0)
-}
-
-// -----------------------------------------------------------------
-
 const Hook = {
 	afterClockTicks: Symbol("Hook/Universe: After a universe lives a cycle"),
 	afterUniverseInstanciatesLink: Symbol("Hook/Link: After a universe instanciates an link"),
@@ -195,9 +100,9 @@ class Link {
 		if (traitConstructor.prototype instanceof Trait || traitConstructor === Trait) {
 			if (!this.traits.has(traitConstructor)) {
 				const trait = new traitConstructor(this)
-				
+
 				trait[Hook.afterLinkInstantiatesTrait](...traitParameters)
-				
+
 				this.traits.add(trait)
 
 				return this[traitConstructor.name] = trait
