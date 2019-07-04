4893e3e90531aabf08e77c195e11761eeeee8f8b
Odepax <odepaxforuma@gmail.com>
2019-07-04 23:00:30 +0200

    Added basic player boosters

--- game.js
+++ game.js
@@ -761,7 +761,7 @@ class Projectile extends Link {
 
 class GatlingBullet extends Projectile {
 	onInitialize(transform) {
-		super.onInitialize(transform, 1000, 4, Tag.enemy, 10)
+		super.onInitialize(transform, 1000, 4, Tag.enemy, 10 * this.universe[Global.damageFactor])
 
 		this.add(GatlingBullet2dRender)
 	}
@@ -769,7 +769,7 @@ class GatlingBullet extends Projectile {
 
 class BlasterBullet extends Projectile {
 	onInitialize(transform) {
-		super.onInitialize(transform, 900, 8, Tag.enemy, 20)
+		super.onInitialize(transform, 900, 8, Tag.enemy, 20 * this.universe[Global.damageFactor])
 
 		this.add(BlasterBullet2dRender)
 	}
@@ -777,7 +777,7 @@ class BlasterBullet extends Projectile {
 
 class ShotgunBullet extends Projectile {
 	onInitialize(transform) {
-		super.onInitialize(transform, 800, 4, Tag.enemy, 15)
+		super.onInitialize(transform, 800, 4, Tag.enemy, 15 * this.universe[Global.damageFactor])
 
 		this.add(ShotgunBullet2dRender)
 	}
@@ -1322,12 +1322,18 @@ const Global = {
 	canvas: Symbol("Global/Link: Universe canvas"),
 	graphics: Symbol("Global/Link: Universe canvas graphics context"),
 	userInteraction: Symbol("Global/Link: Universe user interaction"),
+
 	keyMoveUp: Symbol("Global/Link: Move up key"),
 	keyMoveLeft: Symbol("Global/Link: Move left key"),
 	keyMoveDown: Symbol("Global/Link: Move down key"),
 	keyMoveRight: Symbol("Global/Link: Move right key"),
 	keyShoot: Symbol("Global/Link: Shoot key"),
-	keySpecial: Symbol("Global/Link: Use special capacity key")
+	keySpecial: Symbol("Global/Link: Use special capacity key"),
+
+	damageFactor: Symbol("Global/Trait: Damage factor"),
+	healthFactor: Symbol("Global/Trait: Health factor"),
+	weaponEnergyRegenerationFactor: Symbol("Global/Trait: Weapon energy factor"),
+	capacityEnergyRegenerationFactor: Symbol("Global/Trait: Capacity energy factor")
 }
 
 // -----------------------------------------------------------------
@@ -1359,9 +1365,9 @@ class Player extends Link {
 		this.add(PlayerMovementController)
 
 		this.Collider = this.add(CircleCollider, 27)
-		this.add(Destroyable, 100)
-		this.add(WeaponEnergy, 100)
-		this.add(CapacityEnergy, 100)
+		this.add(Destroyable, 100 * this.universe[Global.healthFactor])
+		this.add(WeaponEnergy, 100, 30 * this.universe[Global.weaponEnergyRegenerationFactor])
+		this.add(CapacityEnergy, 100, 20 * this.universe[Global.capacityEnergyRegenerationFactor])
 
 		this.add(PlayerShip2dRender)
 		this.add(HealthBar2dRender)
