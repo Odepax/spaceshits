f8eddd3a67f2c220e8e056cc3b70446bb6026733
Odepax <odepaxforuma@gmail.com>
2019-08-05 13:22:59 +0200

    Tried to prevent asset caching

--- TestServer.cs
+++ TestServer.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
@@ -39,8 +40,19 @@ namespace Spaceshits
 				{
 					IFileProvider fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
 
+					static void preventCaching(StaticFileResponseContext http)
+					{
+						http.Context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
+						http.Context.Response.Headers.Add("Expires", "-1");
+					}
+
 					app.UseDefaultFiles(new DefaultFilesOptions { FileProvider = fileProvider });
-					app.UseStaticFiles(new StaticFileOptions { FileProvider = fileProvider });
+
+					app.UseStaticFiles(new StaticFileOptions
+					{
+						FileProvider = fileProvider,
+						OnPrepareResponse = preventCaching
+					});
 				})
 				.Build()
 				.Run();
