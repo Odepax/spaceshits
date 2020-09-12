23d7ba52998505a6897f03e6d2406ce82a190a41
Odepax <odepaxforuma@gmail.com>
2020-09-12 19:55:54 +0100

    Refatored flags operations

--- game/lore/tags.js
+++ game/lore/tags.js
@@ -1,3 +1,5 @@
+import { Flag } from "../math/flag.js"
+
 export const Tags = {
 	player:  0b01_000,
 	hostile: 0b10_000,
@@ -6,17 +8,6 @@ export const Tags = {
 	bullet:  0b00_010,
 	field:   0b00_100,
 
-	/** @param {number} a @param {number} b */
-	match(a, b) {
-		const and = a & b
-
-		return and == a || and == b
-	},
-
-	/** @param {number} l @param {number} r */
-	rmatch(l, r) {
-		const and = l & r
-
-		return and == r
-	}
+	contains: Flag.contains, // TODO: remove those
+	match: Flag.matches
 }
---
+++ game/math/flag.js
@@ -0,0 +1,16 @@
+﻿export const Flag = {
+	/** Checks whether `a` is exactly or is more specific than `b`.
+	 * @param {number} a @param {number} b */
+	contains(a, b) {
+		return (a & b) == b
+	},
+
+	/** Checks whether `a` is exactly or is more specific than `b`, or whether `b` is exactly or is more specific than `a`.
+	 * This can be seen as a shortcut for `contains(a, b) || contains(b, a)`.
+	 * @param {number} a @param {number} b */
+	matches(a, b) {
+		const and = a & b
+
+		return and == a || and == b
+	}
+}
